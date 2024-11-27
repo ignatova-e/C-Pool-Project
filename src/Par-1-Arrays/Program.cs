90e046e6454b23754825a40874a7a174d9949a44
Игнатова Евгения <evgeniiaignatova0@gmail.com>
2024-11-27 22:40:45 +0300

    Staart first part of program

---
+++ src/Par-1-Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        // int? size = int.Parse(Console.ReadLine());
+        Console.Write("Введите сколько элементов будет в массиве: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int counter = 0;
+        int[] array = new int[size];
+
+        while (counter != size)
+        {
+            Console.Write("Введите значение для " + (counter + 1)
+            + " элемента массива: ");
+            array[counter] = Convert.ToInt32(Console.ReadLine());
+            counter++;
+        }
+
+
+        int i = 0;
+        while (i != size)
+        {
+            Console.WriteLine(array[i]);
+            i++;
+        }
+    }
+}
+
