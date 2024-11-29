8e404c64bdb3a623ec613ccfa54d7e1fbd118d61
Игнатова Евгения <evgeniiaignatova0@gmail.com>
2024-11-29 22:37:04 +0300

    start second part

---
+++ src/Part-2-Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please text a few words :)  ");
+        string? newList = Console.ReadLine(); // Строка содержащая слова
+        if (newList == null)
+        {
+            Console.WriteLine("You are silly");
+        }
+        else
+        {
+            string[] words = newList.Split(' '); // Строка разбита на массив слов
+            Array.Sort(words);  // Сортиоровка слов по убыванию
+            foreach (string word in words)
+            {
+                Console.WriteLine(word); // Проверка: вывод массива слов
+            }
+        }
+    }
+}
